641c21a2be3c8de272de3aea5a5aeb9c6072f6c1
Collin Miller <collin@freshplanet.com>
2010-04-20 14:31:35 -0400

    delegate each_with_index

--- lib/fauxsql/map_wrapper.rb
+++ lib/fauxsql/map_wrapper.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/module/delegation"
 module Fauxsql
   class MapWrapper < AttributeWrapper
     alias map attribute
-    delegate :[], :each, :keys, :resolve_key, :resolve_value, :to => :map
+    delegate :[], :each, :each_with_index, :keys, :resolve_key, :resolve_value, :to => :map
     
     def []=(key, value)
       dirty! { map[key] = value }
