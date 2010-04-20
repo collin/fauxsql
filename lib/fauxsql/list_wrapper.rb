b08be464cc148f55594031b6b49fd1f0de24744c
Collin Miller <collin@freshplanet.com>
2010-04-20 17:24:32 -0400

    delegate more methods to list wrapper

--- lib/fauxsql/list_wrapper.rb
+++ lib/fauxsql/list_wrapper.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/module/delegation"
 module Fauxsql
   class ListWrapper < AttributeWrapper
     alias list attribute
-    delegate :[], :==, :<<, :first, :last, :equals, :map_resolved, :to => :list
+    delegate :[], :==, :<<, :first, :last, :each, :each_with_index, :equals, :map_resolved, :to => :list
     
     def <<(item)
       dirty! { list << item }
